146a83b2a062606a88736a0c7fca163e9bcf1595
Unknown <clintgubb@gmail.com>
2017-11-14 16:03:53 +1300

    Still a long way to go

---
+++ ToA Voyaging/Assets/RowController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowController : MonoBehaviour {
+
+    // Use this for initialization
+    Animator myAnimator;
+    public Animation myAnimation;
+
+	void Start () {
+        myAnimator = GetComponent<Animator>();
+        myAnimation = GetComponent<Animation>();
+        foreach(AnimationState state in myAnimation)
+        {
+            state.speed = 0.5F;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            myAnimator.Play(0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            myAnimator.Play(0);
+        }
+	}
+}
---
+++ ToA Voyaging/Assets/StartButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.T))
+        {
+            print("Test");
+        }
+
+    }
+
+   public void OnMouseDown()
+    {
+      print("Start button working");
+      SceneManager.LoadScene("Ocean");
+    }
+}
