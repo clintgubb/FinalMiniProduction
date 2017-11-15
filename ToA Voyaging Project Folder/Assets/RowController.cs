517b79a7f16113bc0eaf51cab576b8d62682985a
Unknown <clintgubb@gmail.com>
2017-11-15 16:18:14 +1300

    bleh

--- ToA Voyaging Project Folder/Assets/RowController.cs
+++ ToA Voyaging Project Folder/Assets/RowController.cs
@@ -6,17 +6,26 @@ public class RowController : MonoBehaviour {
 
     // Use this for initialization
     Animator myAnimator;
+    float timer;
+
 
 	void Start () {
         myAnimator = GetComponent<Animator>();
-	}
+        timer = 1.70F;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         if (Input.GetKey(KeyCode.Space))
-        {           
-                myAnimator.Play(0);  
-        }        
+        {
+            if (timer <= 0)
+            {
+                myAnimator.Play(0);
+                timer = 1.70F;
+            }
+                
+        }
+        timer -= Time.deltaTime;
 	}
 }
