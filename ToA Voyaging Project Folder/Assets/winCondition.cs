78daba59ddc5e0adfab3d5ee6778aed0b6626d50
Unknown <imis737@aucklanduni.ac.nz>
2017-11-15 16:18:44 +1300

    added win condition

---
+++ ToA Voyaging Project Folder/Assets/winCondition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winCondition : MonoBehaviour {
+
+    public GameObject boss;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Vector3.Distance(boss.transform.position, transform.position)>200)
+        {
+            //trigger lose
+            //fade to lose screen
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "boss")
+        {
+            //trigger win
+            //fade to win screen
+        }
+    }
+}
