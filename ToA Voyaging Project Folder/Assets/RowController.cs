2b5f5b0f670df50e1e7ca4b48e89079fbe830765
Unknown <clintgubb@gmail.com>
2017-11-15 15:25:18 +1300

    added rythym to the boat

--- ToA Voyaging Project Folder/Assets/RowController.cs
+++ ToA Voyaging Project Folder/Assets/RowController.cs
@@ -6,27 +6,17 @@ public class RowController : MonoBehaviour {
 
     // Use this for initialization
     Animator myAnimator;
-    public Animation myAnimation;
 
 	void Start () {
         myAnimator = GetComponent<Animator>();
-        myAnimation = GetComponent<Animation>();
-        foreach(AnimationState state in myAnimation)
-        {
-            state.speed = 0.5F;
-        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            myAnimator.Play(0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            myAnimator.Play(0);
-        }
+        if (Input.GetKey(KeyCode.Space))
+        {           
+                myAnimator.Play(0);  
+        }        
 	}
 }
